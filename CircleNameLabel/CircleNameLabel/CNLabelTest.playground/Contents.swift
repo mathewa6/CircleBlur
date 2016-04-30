d4184fe670b7a61cec28ef581d5a6792ce1a88e4
Adi <mathewa6@users.noreply.github.com>
2016-04-29 23:11:04 -0400

    Added playground to test swift version of the label, but Vibrancy isn't working. Found out why the bug mentioned in README existed and edited README with the answer. RTFM next time.

---
+++ CircleNameLabel/CircleNameLabel/CNLabelTest.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+let chars = "AB"
+
+func idealSize(font: UIFont, _ rect: CGRect, _ text: String) -> CGFloat {
+    let ppp = font.lineHeight/font.pointSize
+    var idealPointSize = rect.size.height * ppp
+    
+    if text.characters.count == 1 {
+        idealPointSize *= 0.6
+    } else {
+        idealPointSize = 0.75 * idealPointSize /  CGFloat(text.characters.count)
+    }
+    
+    return idealPointSize
+}
+
+let bg = UIView(frame: CGRectMake(0, 0, 450, 450))
+bg.backgroundColor = UIColor.redColor()
+
+var baseView = UIView(frame: CGRectMake(112.5, 112.5, 225, 225))
+baseView.layer.cornerRadius = baseView.bounds.size.width/2.01
+baseView.clipsToBounds = true
+//baseView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.25)
+
+var blurEffect = UIBlurEffect(style: .Light)
+var blurView = UIVisualEffectView(effect: blurEffect)
+blurView.frame = baseView.bounds
+//blurView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+
+let size: CGFloat = idealSize(UIFont.systemFontOfSize(100.0), baseView.bounds, chars)
+
+var text = UILabel(frame: baseView.bounds)
+text.font = UIFont.systemFontOfSize(size)
+text.textColor = UIColor.whiteColor()
+text.textAlignment = .Center
+text.backgroundColor = UIColor.clearColor()
+text.text = chars
+
+//blurView.addSubview(text)
+baseView.addSubview(blurView)
+
+var vibEffect = UIVibrancyEffect(forBlurEffect: blurEffect)
+var vibView = UIVisualEffectView(effect: vibEffect)
+vibView.frame = blurView.bounds
+
+blurView.contentView.addSubview(vibView)
+vibView.contentView.addSubview(text)
+
+
+bg.addSubview(baseView)
--- README.md
+++ README.md
@@ -23,6 +23,16 @@ Alternatively, simply drop a UIView into a storyboard VC and set it's class in t
 
 Setting property labelRadiusFactor which is the the base View's layer.cornerRadius property to 2.0, which calculates the bounds.size.width/2.0, causes UIBlurEffect to not function properly. Why? 
 
+*ANSWER:* Because the docs for UIVisualEffectView clearly state that changing the alpha of the same will result in undefined behaviour. Removing the call to 
+
+
+```obj-c
+
+blurView.backgroundColor = [self.color colorWithAlphaComponent:self.colorAlpha];
+
+```
+fixes the issue even when labelRadiusFactor is 2.0.
+
 ## Screenshots ##
 
 ![Image of proper labels](https://raw.githubusercontent.com/mathewa6/CircleBlur/master/Screenshots/Screen%20Shot%202016-04-29%20at%202.55.15%20PM.png)
